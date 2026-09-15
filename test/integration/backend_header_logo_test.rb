89bfc837b71cdb65d3b2d53ef909bd9f28afc5a1
Marc Sutton <marc@codev.uk>
2026-09-15 20:33:10 +0100

    Admin header: company logo top left with the app logo as fallback

---
+++ test/integration/backend_header_logo_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+# The admin header shows the company logo from General Settings, falling back
+# to the app logo when none is uploaded.
+class BackendHeaderLogoTest < ActionDispatch::IntegrationTest
+  LOGO = "data:image/png;base64,iVBORw0KGgo=".freeze
+
+  def login_admin
+    post "/login/validate", params: { username: "administrator", password: "administrator1" }
+  end
+
+  test "the company logo is shown top left" do
+    Setting.set("company_logo", LOGO)
+    login_admin
+    get "/calendar"
+    assert_select "#header-logo img[src=?]", LOGO
+  end
+
+  test "the app logo is shown when no company logo is set" do
+    Setting.set("company_logo", "")
+    login_admin
+    get "/calendar"
+    assert_select "#header-logo img[src*=?]", "/assets/logo"
+  end
+end
